158c68d035fd07f37087d41dc7d5778b57af4584
Arrieta Cristian <arrieta.cristian.gabriel@gmail.com>
2017-11-27 17:32:03 -0300

    test1 rojo

--- src/test/java/arrieta/com/testBowling.java
+++ src/test/java/arrieta/com/testBowling.java
@@ -1,7 +1,27 @@
 package arrieta.com;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
 import junit.framework.TestCase;
 
-public class testBowling extends TestCase {
+public class testBowling {
+
+	private Frame fr;
+	@Before	
+	public void setUp() throws Exception {
+		fr = new Frame();
+	}
+	
+	@Test
+	public void FrameValido() {
+		
+		fr.valido(2,3);
+		junit.framework.Assert.assertTrue(true);
+	}
+
+	
 
 }
