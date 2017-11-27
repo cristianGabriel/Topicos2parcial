18b20ac69a297716947785355758bd3f79439471
Arrieta Cristian <arrieta.cristian.gabriel@gmail.com>
2017-11-27 17:37:55 -0300

    test1 verde de nuevo

--- src/test/java/arrieta/com/Frame.java
+++ src/test/java/arrieta/com/Frame.java
@@ -3,7 +3,13 @@ package arrieta.com;
 public class Frame {
 	
 	public boolean valido(int i, int j) {
-		return true;
+		int frame = i +j;
+		
+		if (frame >=0 && frame <=10) {
+			return true;
+		}
+		
+		return false;
 		
 	}
 
